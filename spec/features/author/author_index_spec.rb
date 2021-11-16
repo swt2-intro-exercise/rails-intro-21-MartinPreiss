0a21a90d4fadbb5772251b23fb683c4c2192fc8d
MartinPreiss <user@Martins-Bude.localdomain>
2021-11-16 22:38:50 +0100

    author test

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -24,5 +24,9 @@ describe "author index page", type: :feature do
         expect(page).to have_link 'New', href: new_author_path
         
        end
-
+    it "Author index page should link to authors' details page #10" do
+        @alan = FactoryBot.create :author
+        visit authors_path
+        expect(page).to have_link 'Show', href: author_path(@alan)
+       end
  end
