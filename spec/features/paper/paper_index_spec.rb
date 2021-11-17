b265bdf8cf7b957da0b087841b47a955ddc3d105
MartinPreiss <user@Martins-Bude.localdomain>
2021-11-18 00:57:11 +0100

    author test

--- spec/features/paper/paper_index_spec.rb
+++ spec/features/paper/paper_index_spec.rb
@@ -8,4 +8,17 @@ describe "paper index page", type: :feature do
      visit papers_path
     end
 
+    it "Paper index page should allow filtering out papers by year" do
+      paper1 = Paper.create({year: 1950, venue: "Test", title:"wichtiges paper1"})
+      paper2 = Paper.create({year: 1960, venue: "Test", title:"wichtiges paper2"})
+      visit papers_path(year:"1950")
+
+      expect(page).to have_text("1950")
+      expect(page).not_to have_text("1960")
+
+
+
+     end
+ 
+
 end
